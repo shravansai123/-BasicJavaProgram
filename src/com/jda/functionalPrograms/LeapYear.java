351d322ad43e1f64be9a9ada1b2ffaaac0b0006e
shravansai123 <ksai.reddy@jda.com>
2018-07-04 17:22:53 +0530

    leap year

---
+++ src/com/jda/functionalPrograms/LeapYear.java
@@ -0,0 +1,16 @@
+package com.jda.functionalPrograms;
+
+import com.jda.utility.Utility;
+
+public class LeapYear {
+public static void main(String args[]){
+	System.out.println("Enter the Year ");
+	Utility util=new Utility();
+	int year=util.getNoOfFlips();
+	boolean isLeapYear=util.isLeapyear(year);
+	if(isLeapYear)
+		System.out.println(year +" is Leap Year" );
+	else
+		System.out.println(year +" is not Leap Year" );
+}
+}
--- src/com/jda/utility/Utility.java
+++ src/com/jda/utility/Utility.java
@@ -16,6 +16,18 @@ public int getNoOfFlips(){
 	int numberOfFlips=scanner.nextInt();
 	return numberOfFlips;
 }
+public boolean isLeapyear(int year){
+	boolean check;
+	if (year%400==0)
+		check=true;
+	else if(year%100==0)
+		check=false;
+	else if(year%4==0)
+		check=true;
+	else
+		check=false;
+	return check;
+}
 public String replaceStr(String template,String name){
 	template=template.replaceAll("<<UserName>>",name);
 	return template;
